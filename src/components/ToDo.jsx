27d00629b379d9762a6d2d6e4eae0c1c2f8dc1bd
felipefraga <felifraga10@gmail.com>
2023-10-26 16:13:47 -0300

    localStorage

--- src/components/ToDo.jsx
+++ src/components/ToDo.jsx
@@ -1,16 +1,19 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPenToSquare, faTrash } from '@fortawesome/free-solid-svg-icons'
+import { faPenToSquare, faTrash, faCheckSquare, faSquare } from '@fortawesome/free-solid-svg-icons'
 
 export const ToDo = ({ task, toggleComplete, deleteToDo, editToDo }) => {
     return (
-        <div className={`flex m-4 border rounded p-2 bg-indigo-00 justify-between items-center ${task.completed ? 'bg-indigo-100 border-none text-indigo-900 dark:bg-gray-900' : ''}`}>
+        <div className={`flex m-4 border rounded p-2 justify-between items-center ${task.completed ? 'border-none text-indigo-900 dark:bg-gray-900 dark:text-indigo-400' : ''}`}>
             <p onClick={() => toggleComplete(task.id)}>{task.task}</p>
             <div>
-                <FontAwesomeIcon icon={faPenToSquare} onClick={() => editToDo(task.id)} className='pr-2' />
-                <FontAwesomeIcon icon={faTrash} onClick={() => deleteToDo(task.id)} />
+                <FontAwesomeIcon
+                    icon={task.completed ? faCheckSquare : faSquare}
+                    onClick={() => toggleComplete(task.id)}
+                    className={`pr-2 ${task.completed ? 'text-indigo-900 dark:text-indigo-400' : 'text-indigo-500 dark:text-indigo-300'}`}
+                />
+                <FontAwesomeIcon icon={faPenToSquare} onClick={() => editToDo(task.id)} className='pr-2 text-indigo-500 dark:text-indigo-300' />
+                <FontAwesomeIcon icon={faTrash} onClick={() => deleteToDo(task.id)} className='text-indigo-500 dark:text-indigo-300' />
             </div>
         </div>
-
-
-    )
+    );
 }
